9b5cce76f1e0fd327356fc71d125628174564841
Caiozatski <caiozatroski123@gmail.com>
2026-08-18 11:55:09 -0300

    Finished Who we are page. Implemented bg-color change on header and started contact-us page

---
+++ JalapenoBrothers/js/index.js
@@ -0,0 +1,11 @@
+var className = "change-color";
+var scrollTrigger = 60;
+
+window.onscroll = function() {
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger){
+    document.getElementsByTagName("header")[0].classList.add(className)
+  }
+  else {
+    document.getElementsByTagName("header")[0].classList.remove(className)
+  }
+}
