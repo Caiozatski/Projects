cdcfbb831d87cc3abb88b4dfcf270666e71ee7c3
Caiozatski <caiozatroski123@gmail.com>
2026-09-02 17:11:08 -0300

    Hero background improvement, automatic pass images also can work with button clicks

--- Evertrip/scripts/hero-carrosel.js
+++ Evertrip/scripts/hero-carrosel.js
@@ -1,12 +1,60 @@
-let currentSlide = 1;
+const carousel = document.querySelector(".hero_backgrounds");
+const slides = document.querySelectorAll(".hero_backgrounds > div");
+const buttons = document.querySelectorAll(".labels label");
 
-setInterval (() =>{
-  currentSlide++;
+let currentSlide = 0;
+let autoPlay;
 
-  if (currentSlide > 4) {
-    currentSlide = 1;
+
+function startAutoPlay() {
+  autoPlay = setInterval(() => {
+    currentSlide++;
+
+    if (currentSlide >= slides.length) {
+      currentSlide = 0;
+    }
+
+    moveCarousel();
+    updateButtons();
+  }, 5000);
+}
+
+startAutoPlay();
+
+function moveCarousel() {
+
+  let position = -currentSlide * 20;
+  carousel.style.transform = `translateX(${position}%)`
+}
+
+carousel.addEventListener("transitionend", () => {
+  if (currentSlide === 4) {
+    carousel.style.transition = "none";
+    currentSlide = 0;
+    carousel.style.transform = "translateX(0%)";
+    updateButtons();
+
+    setTimeout(() => {
+      carousel.style.transition = "all .5s linear";
+    }, 0);
   }
+});
 
-  document.querySelector(`#slider${currentSlide}`).checked = true;
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    clearInterval(autoPlay);
+    currentSlide = index;
+    moveCarousel();
+    updateButtons();
+    startAutoPlay();
+  })
+})
 
-}, 5000)
+function updateButtons() {
+  buttons.forEach((button, index) => {
+    button.classList.remove("active");
+    if (index == currentSlide) {
+      button.classList.add("active");
+    }
+  });
+}
